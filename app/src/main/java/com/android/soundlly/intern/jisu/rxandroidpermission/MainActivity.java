278495e0aa0382ae32b9175efc00039b4ce7b1ec
sang <mim3431@naver.com>
2017-03-03 15:31:59 +0900

    Initial commit

---
+++ app/src/main/java/com/android/soundlly/intern/jisu/rxandroidpermission/MainActivity.java
@@ -0,0 +1,13 @@
+package com.android.soundlly.intern.jisu.rxandroidpermission;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
